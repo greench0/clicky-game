a6931be10676dc9d0c34f5930ff539c809b26013
chris green <greench@gmail.com>
2019-02-15 21:49:44 -0800

    Updates

---
+++ precache-manifest.0f9214849c64d5ee61a9569f142da474.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "8139227a5e151cc1d2e5",
+    "url": "/clicky-game/static/js/main.8139227a.chunk.js"
+  },
+  {
+    "revision": "838835256c241f9b6b64",
+    "url": "/clicky-game/static/js/2.83883525.chunk.js"
+  },
+  {
+    "revision": "2c8788fd2f1434ec6063f1a35b3352bf",
+    "url": "/clicky-game/index.html"
+  }
+];
